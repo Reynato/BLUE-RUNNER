0ef2f1cd1a81f48a5b1b6014a761c6f37f1834f0
itoh <yuki@frontartgraph.com>
2022-03-14 15:48:31 +0900

    edit: wip 20220314

--- app/plugins/imagemin.mjs
+++ app/plugins/imagemin.mjs
@@ -4,14 +4,16 @@ import imageminPngquant from "imagemin-pngquant";
 import imageminGifsicle from "imagemin-gifsicle";
 import imageminSvgo from "imagemin-svgo";
 
-imagemin(["./app/static/**/*.{jpg,png,gif,svg}"], {
-  destination: "./build/images",
+const files = await imagemin(["./app/static/**/*.{jpg,png,gif,svg}"], {
+  destination: "../build/images",
   plugins: [
     imageminMozjpeg({ quality: 80 }),
     imageminPngquant({ quality: [0.6, 0.8] }),
     imageminGifsicle(),
     imageminSvgo(),
   ],
-}).then(() => {
-  console.log("Images optimized");
+});
+
+files.forEach((item) => {
+  console.log(item.path);
 });
