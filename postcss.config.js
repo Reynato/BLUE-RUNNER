e9ab2eeb572597c496ad939e40c83efc07cc79d4
itoh <yuki@frontartgraph.com>
2022-09-13 15:40:01 +0900

    🖋: Changed to 20220913 15:40:01

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require("autoprefixer")(), require("postcss-logical")(["padding-inline", "inset"]), require("flex-gap-polyfill")],
+  plugins: [
+    require("autoprefixer")(),
+    require("postcss-logical")(["padding-inline", "inset"]),
+    require("flex-gap-polyfill"),
+  ],
 };
