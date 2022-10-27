d1a1500b0a95fcae7108649f8942e82282ae0a03
itoh <yuki@frontartgraph.com>
2022-10-27 22:54:50 +0900

    🖋: Changed to 20221027 22:54:50

--- app/assets/javascripts/basicAuth.js
+++ app/assets/javascripts/basicAuth.js
@@ -1,11 +1,11 @@
 function basicAuth() {
-  const dateToYyyymmdd = (date) => {
+  const dateToYYYYMMDD = (date) => {
     let n = date.getFullYear() * 10000;
     n += date.getMonth() * 100 + 100;
     n += date.getDate();
     return ("000" + n).slice(-8);
   };
-  const setPass = "reynato" + dateToYyyymmdd(new Date());
+  const setPass = `reynato${dateToYYYYMMDD(new Date())}`.toUpperCase();
   const whiteURL = ["http://localhost:3000"];
   const htmlElStyle = document.querySelector("html").style;
   htmlElStyle.opacity = 0;
@@ -20,7 +20,8 @@ function basicAuth() {
     return;
   }
   window.setTimeout(() => {
-    const password = window.prompt("パスワードを入力してください", "");
+    let password = window.prompt("パスワードを入力してください", "");
+    password = password.toUpperCase();
     if (password === setPass) {
       localStorage.setItem("fakeAuthStr", password);
       htmlElStyle.opacity = 1;
@@ -28,8 +29,7 @@ function basicAuth() {
       htmlElStyle.opacity = 0;
       htmlElStyle.pointerEvents = "none";
       window.alert("パスワードが違います");
-      document.querySelector("html").remove();
     }
-  }, 500);
+  }, 100);
 }
 basicAuth();
